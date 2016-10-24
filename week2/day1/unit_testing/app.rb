5607580adb246096fa9fe11cd006b83bb7199c0c
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-10-24 09:05:49 -0400

    Unit Testing: Add unit testing structure

---
+++ week2/day1/unit_testing/app.rb
@@ -0,0 +1,37 @@
+# app.rb
+
+the_calculator = StringCalculator.new
+
+p the_calculator.add("7,7") == 14
+p the_calculator.add("8,4") == 12
+
+p the_calculator.add("6") == 6
+p the_calculator.add("3") == 3
+
+p the_calculator.add("") == 0
+
+p the_calculator.add == 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# BONUS:
+p the_calculator.add("7$7") == 14
+p the_calculator.add("8$4") == 12
+
+p the_calculator.add("8$4") == 12
+
+p the_calculator.add("7,7,1,2") == 17
+p the_calculator.add("7$7$1$2") == 17
+
+p the_calculator.add("7$7,1,2") == 17
