7eeea7c940d12b12eb5505bb6a3a808e4a176be4
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-10-24 12:45:23 -0400

    Unit Testing: Update spec

--- week2/day1/unit_testing/lib/string_calculator.rb
+++ week2/day1/unit_testing/lib/string_calculator.rb
@@ -1,5 +1,16 @@
 class StringCalculator
-  def add(numbers_string)
+  def add(numbers_string = "")
+    #  0 1 2
+    # "8 , 4"
+
+    #  0   1   2
+    # "6" nil nil
+
+    #     0   1   2
+    # "" nil nil nil
+
+    # nil.to_i == 0
+
     numbers_string[0].to_i + numbers_string[2].to_i
   end
 end
--- week2/day1/unit_testing/spec/string_calculator_spec.rb
+++ week2/day1/unit_testing/spec/string_calculator_spec.rb
@@ -8,29 +8,36 @@ require_relative("../lib/string_calculator.rb")
 RSpec.describe StringCalculator do
   describe "#add" do
 
-    it "adds two numbers separated by commas" do
-      the_calc = StringCalculator.new
+    before :each do
+      @the_calc = StringCalculator.new
+    end
 
+
+    it "adds two numbers separated by commas" do
       # p the_calculator.add("7,7")  ==  14
-      expect(the_calc.add("7,7")).to  eq(14)
+      expect(@the_calc.add("7,7")).to  eq(14)
 
 
       # p the_calculator.add("8,4") == 12
-      expect(the_calc.add("8,4")).to  eq(12)
+      expect(@the_calc.add("8,4")).to  eq(12)
     end
 
     it "returns the number for a single number" do
-      # assertions
+      # p the_calculator.add("6") == 6
+      expect(@the_calc.add("6")).to eq(6)
+
+      # p the_calculator.add("3") == 3
+      expect(@the_calc.add("3")).to eq(3)
     end
 
     it "returns 0 for the empty string" do
-      # assertions
+      # p the_calculator.add("") == 0
+      expect(@the_calc.add("")).to eq(0)
     end
 
     it "returns 0 when no argument given" do
-      # assertions
+      # p the_calculator.add == 0
+      expect(@the_calc.add).to eq(0)
     end
-
-    # etc.
   end
 end
