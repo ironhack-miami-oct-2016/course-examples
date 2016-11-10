acbdd5b226864e71418cefd186c2ae15672bceb2
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-11-10 09:10:19 -0500

    Timetracking: Add validations

--- week4/timetracking/app/models/project.rb
+++ week4/timetracking/app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ApplicationRecord
   has_many :time_entries
     # project_id of the TimeEntry model
+
+  validates :name, { presence: true, uniqueness: true }
+  validates :name, { length: { maximum: 30 } }
+  validates :name, { format: /\A[a-zA-Z0-9 .']+\z/ }
 end
--- week4/timetracking/app/models/time_entry.rb
+++ week4/timetracking/app/models/time_entry.rb
@@ -2,4 +2,8 @@ class TimeEntry < ApplicationRecord
   belongs_to :project
     # project_id
     # id of the Project model
+
+  validates :hours, { numericality: true }
+  validates :minutes, { numericality: true }
+  validates :date, { presence: true }
 end
