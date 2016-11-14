db5f22c3ab645c3b2a804be6bb1b8725b2f76df6
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-11-14 14:34:06 -0500

    jQuery: Add logic examples

--- week5/day1/jquery/js/app.js
+++ week5/day1/jquery/js/app.js
@@ -16,23 +16,86 @@ $(document).ready(function () {
 
   $("#cookies").on("click", cookieMessage);
 
+  $("#ice-cream").on("click", iceCreamMessage);
+
+  $(".js-change-unicorn").on("click", changeUnicorn);
+
+  $(".js-button-color").on("click", changeButtonColor);
+
 });
 
 
 
+function changeButtonColor () {
+  $("button").addClass("btn");
+
+  if ( $("button").hasClass("btn-success") ) {
+    $("button").removeClass("btn-success");
+    $("button").addClass("btn-info");
+  }
+
+  else if ( $("button").hasClass("btn-info") ) {
+    $("button").removeClass("btn-info");
+    $("button").addClass("btn-warning");
+  }
+
+  else if ( $("button").hasClass("btn-warning") ) {
+    $("button").removeClass("btn-warning");
+  }
+
+  else {
+    $("button").addClass("btn-success");
+  }
+}
+
+
+
+
+
+
+var unicornCount = 2;
+
+function changeUnicorn () {
+  unicornCount += 1;
+
+  if (unicornCount > 3) {
+    unicornCount = 1;
+  }
+
+  $(".js-unicorn-image").attr("src", `images/unicorn${unicornCount}.gif`);
+}
+
+
+function iceCreamMessage () {
+  var content = `
+    <i> Ice cream 🍦 </i>`;
+
+  $(".js-jq-paragraph").html(content);
+}
+
+
+
 function cookieMessage () {
-  var cookieContent = `
-    <div class="popup">
-      <h2> 🍪 Have some cookies. 🍪 </h2>
 
-      <p> Cookies are good for you. Here's why: </p>
+  if ($(".js-cookie-div").length > 0) {
+    $(".js-cookie-div").remove();
+  }
 
-      <ul>
-        <li> Cookies taste good. </li>
-        <li> They have 0 calories. </li>
-        <li> Cookies don't lie to you. </li>
-      </ul>
-    </div>`;
+  else {
+    var cookieContent = `
+      <div class="popup js-cookie-div">
+        <h2> 🍪 Have some cookies. 🍪 </h2>
 
-  $("body").append(cookieContent);
+        <p> Cookies are good for you. Here's why: </p>
+
+        <ul>
+          <li> Cookies taste good. </li>
+          <li> They have 0 calories. </li>
+          <li> Cookies don't lie to you. </li>
+        </ul>
+      </div>`;
+
+    $("body").append(cookieContent);
+  }
+
 }
