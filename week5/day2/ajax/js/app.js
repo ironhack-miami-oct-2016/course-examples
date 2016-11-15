16baf7b08dd257b2815e3e20d114ea58b71bbcee
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-11-15 12:14:49 -0500

    AJAX: Add Admiral Ackbar form

--- week5/day2/ajax/js/app.js
+++ week5/day2/ajax/js/app.js
@@ -4,8 +4,42 @@ console.log("APP JS READY");
 $(document).ready(function () {
 
   $(".js-fetch-characters").on("click", fetchCharacters);
+  $(".js-ackbar-form").on("submit", saveAckbar);
 
 });
+
+
+function saveAckbar (eventThing) {
+  eventThing.preventDefault();
+
+  var weaponInput = $(".js-ackbar-weapon").val();
+
+  if (weaponInput === "") {
+    weaponInput = "Memes";
+  }
+
+  var ackbarInfo = {
+    name: "Admiral Ackbar",
+    occupation: "Rebel Fleet Admiral",
+    weapon: weaponInput,
+  };
+
+  $.ajax({
+    type: "POST",
+    url: "https://ironhack-characters.herokuapp.com/characters",
+    data: ackbarInfo,
+    success: showAckbar,
+    error: handleError,
+  });
+}
+
+
+function showAckbar (infoFromApi) {
+  console.log("Save Ackbar success");
+  console.log(infoFromApi);
+}
+
+
 
 
 function fetchCharacters () {
@@ -21,6 +55,8 @@ function fetchCharacters () {
 function showCharacters (x) {
   console.log("Fetch characters success");
   console.log(x);
+
+  $(".js-character-list").empty();
 
   x.forEach(function (oneCharacter) {
     var characterItem = `
