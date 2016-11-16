8a161fda5ebd9ca59654b64d12f4464a9019ea66
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-11-16 10:55:45 -0500

    Geolocation: Add watch position

--- week5/day3/geolocation/js/app.js
+++ week5/day3/geolocation/js/app.js
@@ -4,14 +4,53 @@ $(document).ready(function () {
 
   console.log("The page has finished loading.");
   promptForLocation();
+  beginWatch();
 
 });
+
+
+function beginWatch () {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.watchPosition(watchSuccess, watchError);
+  }
+}
+
+
+function watchSuccess (locInfo) {
+  console.log("Watch location:");
+  console.log(locInfo);
+
+  var now = new Date(locInfo.timestamp);
+  var time = now.toTimeString();
+
+  var watchItem = `
+    <li>
+      Latitude: ${locInfo.coords.latitude}
+      <br>
+      Longitude: ${locInfo.coords.longitude}
+      <br>
+      Time: ${time}
+    </li>`;
+
+  $(".js-watch-list").append(watchItem);
+}
+
+function watchError (errInfo) {
+  console.log("Watch error:");
+  console.log(errInfo);
+}
+
 
 
 
 function promptForLocation () {
   if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(showPositionInDom, showError);
+    var optionsThing = {
+      enableHighAccuracy: true,
+      timeout: 5000
+    };
+
+    navigator.geolocation.getCurrentPosition(showPositionInDom, showError, optionsThing);
   }
 
   else {
