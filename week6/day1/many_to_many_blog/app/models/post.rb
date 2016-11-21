cae42e0f51f625f532b4ba6e5144352efdf935be
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-11-21 16:28:17 -0500

    Many To Many: Add comments to has_many through

--- week6/day1/many_to_many_blog/app/models/post.rb
+++ week6/day1/many_to_many_blog/app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  has_many :post_tags
+    #           -----------------
+    #           |               |
+  has_many :post_tags        #  |
   has_many :tags, through: "post_tags"
+    # PostTag --> post_tags
 end
--- week6/day1/many_to_many_blog/app/models/tag.rb
+++ week6/day1/many_to_many_blog/app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ApplicationRecord
-  has_many :post_tags
+    #           ------------------
+    #           |                |
+  has_many :post_tags         #  |
   has_many :posts, through: "post_tags"
+    # PostTag --> post_tags
 end
