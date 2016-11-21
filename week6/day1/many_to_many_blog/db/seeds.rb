adbed06901768795bda8f6a668b84c3fbe501cce
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-11-21 16:10:30 -0500

    Many to Many: Update seeds

--- week6/day1/many_to_many_blog/db/seeds.rb
+++ week6/day1/many_to_many_blog/db/seeds.rb
@@ -7,27 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-user1 = User.create!(name: "blah")
-user2 = User.create!(name: "blah")
-user3 = User.create!(name: "blah")
+user1 = User.create!(name: "Ariel")
+user2 = User.create!(name: "Chenje")
+user3 = User.create!(name: "Xiomara")
 
 
-        user1.posts.create!(title: "TITLE", text: "Man bun keytar cred hot chicken. Humblebrag sartorial post-ironic, pabst lo-fi sriracha normcore literally hell of jianbing organic cronut forage unicorn church-key. Kombucha lomo man braid, man bun letterpress etsy everyday carry occupy flexitarian.")
-postA = user1.posts.create!(title: "TITLE", text: "Keffiyeh brunch squid, pop-up vape schlitz gluten-free banjo. Fanny pack leggings selfies vinyl, gochujang post-ironic heirloom enamel pin lomo 90's. ")
+        user1.posts.create!(title: "This week in food", text: "Man bun keytar cred hot chicken. Humblebrag sartorial post-ironic, pabst lo-fi sriracha normcore literally hell of jianbing organic cronut forage unicorn church-key. Kombucha lomo man braid, man bun letterpress etsy everyday carry occupy flexitarian.")
+postA = user1.posts.create!(title: "Destroying n00bs", text: "Keffiyeh brunch squid, pop-up vape schlitz gluten-free banjo. Fanny pack leggings selfies vinyl, gochujang post-ironic heirloom enamel pin lomo 90's. ")
 
-        user2.posts.create!(title: "TITLE", text: "Bushwick leggings trust fund, green juice gentrify man braid kickstarter mustache DIY health goth.")
-postB = user2.posts.create!(title: "TITLE", text: "Green juice VHS craft beer photo booth bushwick iPhone, franzen fam organic authentic flexitarian helvetica.")
+        user2.posts.create!(title: "Using Paperclip to manage images in Rails 5", text: "Bushwick leggings trust fund, green juice gentrify man braid kickstarter mustache DIY health goth.")
+postB = user2.posts.create!(title: "To bootcamp or not to bootcamp", text: "Green juice VHS craft beer photo booth bushwick iPhone, franzen fam organic authentic flexitarian helvetica.")
 
-        user3.posts.create!(title: "TITLE", text: "Live-edge irony occupy hammock, pop-up la croix shabby chic vape cliche tumeric aesthetic hashtag biodiesel vaporware.")
-postC = user3.posts.create!(title: "TITLE", text: "Seitan health goth craft beer, crucifix wayfarers VHS next level skateboard.")
+        user3.posts.create!(title: "Traveling nica-smart", text: "Live-edge irony occupy hammock, pop-up la croix shabby chic vape cliche tumeric aesthetic hashtag biodiesel vaporware.")
+postC = user3.posts.create!(title: "Granada's sustainable tourism", text: "Seitan health goth craft beer, crucifix wayfarers VHS next level skateboard.")
 
 
-tagA = Tag.create!(name: "TAGNAME")
-tagB = Tag.create!(name: "TAGNAME")
-tagC = Tag.create!(name: "TAGNAME")
-tagD = Tag.create!(name: "TAGNAME")
-tagE = Tag.create!(name: "TAGNAME")
-tagF = Tag.create!(name: "TAGNAME")
+tagA = Tag.create!(name: "Video Games")
+tagB = Tag.create!(name: "n00bs")
+tagC = Tag.create!(name: "Web Development")
+tagD = Tag.create!(name: "Rails")
+tagE = Tag.create!(name: "Sustainability")
+tagF = Tag.create!(name: "Tourism")
 
 
 PostTag.create!(post_id: postA.id, tag_id: tagA.id)
@@ -38,5 +38,8 @@ PostTag.create!(post: postB, tag: tagC)
 PostTag.create!(post: postB, tag: tagD)
 
 
-postC.tags.push( tagE )
-postC.tags.push( tagF )
+postC.tags.push(tagE)
+postC.tags.push(tagF)
+
+
+puts "Seeds done"
